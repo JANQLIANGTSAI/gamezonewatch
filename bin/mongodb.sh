20d1a73f5f8679df3e8a41f70ebe3487f54a473e
Max Tsai <mt8168@gmail.com>
2017-05-07 14:24:26 -0700

    clean up

--- bin/mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-# run manually
-ulimit -n 1024 && mongod --config /usr/local/etc/mongod.conf
-
-# To have launchd start mongodb now and restart at login:
-# $  brew services start mongodb
